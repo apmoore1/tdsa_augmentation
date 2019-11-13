4745ba65eb7384fff259f8d7cf7b13a283f7c26b
Andrew Moore <andrew.p.moore94@gmail.com>
2019-11-13 10:04:35 +0000

    Corrected comment

--- tdsa_augmentation/data_augmentation/extract_predicted_targets.sh
+++ tdsa_augmentation/data_augmentation/extract_predicted_targets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Extracting targets from the Laptop domain"
-#python ./tdsa_augmentation/data_augmentation/extract_predicted_targets.py ./resources/predicted_targets/laptop.json ./data/original_laptop_sentiment/train.json 0.9 ./resources/target_words/laptop_predicted.txt
+python ./tdsa_augmentation/data_augmentation/extract_predicted_targets.py ./resources/predicted_targets/laptop.json ./data/original_laptop_sentiment/train.json 0.9 ./resources/target_words/laptop_predicted.txt
 echo "Extracting targets from the Restaurant domain"
 python ./tdsa_augmentation/data_augmentation/extract_predicted_targets.py ./resources/predicted_targets/restaurant.json ./data/original_restaurant_sentiment/train.json 0.9 ./resources/target_words/restaurant_predicted.txt
 echo "Extracting targets from the Election domain"
